ea084350e38cd7cb7d5e9926df3ae72bd6468e53
olhabachurina <132917766+olhabachurina@users.noreply.github.com>
2023-09-27 20:28:17 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_09_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix2D<int> matrix1 = new Matrix2D<int>(2, 2);
+            Matrix2D<int> matrix2 = new Matrix2D<int>(2, 2);
+
+            matrix1[0, 0] = 1;
+            matrix1[0, 1] = 2;
+            matrix1[1, 0] = 3;
+            matrix1[1, 1] = 4;
+
+            matrix2[0, 0] = 5;
+            matrix2[0, 1] = 6;
+            matrix2[1, 0] = 7;
+            matrix2[1, 1] = 8;
+
+            Matrix2D<int> sumMatrix = matrix1 + matrix2;
+            
+
+            Console.WriteLine("Sum of matrices:");
+            for (int i = 0; i < sumMatrix.Rows; i++)
+            {
+                for (int j = 0; j < sumMatrix.Columns; j++)
+                {
+                    Console.Write(sumMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+
+
+
+
+
+
+}
+
---
+++ matrix2d.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _27_09_23
+{
+    class Matrix2D<T>
+    {
+        private T[,] data;
+
+        public int Rows { get; private set; }
+        public int Columns { get; private set; }
+
+        public T[,] Arr
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public int Size
+        {
+            get { return Rows * Columns; }
+        }
+
+        public Matrix2D(int rows, int columns)
+        {
+            Rows = rows;
+            Columns = columns;
+            data = new T[rows, columns];
+        }
+
+        public T this[int row, int column]
+        {
+            get { return data[row, column]; }
+            set { data[row, column] = value; }
+        }
+
+        public static Matrix2D<T> operator +(Matrix2D<T> matrix1, Matrix2D<T> matrix2)
+        {
+            if (matrix1.Rows != matrix2.Rows || matrix1.Columns != matrix2.Columns)
+            {
+                throw new ArgumentException("Matrices must have the same dimensions for addition.");
+            }
+
+            Matrix2D<T> result = new Matrix2D<T>(matrix1.Rows, matrix1.Columns);
+            for (int i = 0; i < matrix1.Rows; i++)
+            {
+                for (int j = 0; j < matrix1.Columns; j++)
+                {
+                    dynamic value1 = matrix1[i, j];
+                    dynamic value2 = matrix2[i, j];
+                    result[i, j] = value1 + value2;
+                }
+            }
+            return result;
+        }
+    }
+}
